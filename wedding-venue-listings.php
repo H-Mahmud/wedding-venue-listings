c4ca9223b14145cb8e1149b336d7faeb4b9c00f9
Mahmud <me@imahmud.com>
2024-11-06 23:09:49 +0600

    plugin init

---
+++ wedding-venue-listings.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Wedding Venue Listings
+ * Plugin URI:        https://github.com/H-Mahmud/wedding-venue-listings
+ * Description:       A versatile plugin for wedding venue listings, enabling owners to create detailed profiles with media and booking info. Visitors can search, filter by category and location, view ratings, and leave reviews.
+ * Version:           1.0
+ * Requires at least: 6.6
+ * Requires PHP:      7.2
+ * Author:            Mahmudul Hasan
+ * Author URI:        https://imahmud.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wedding-venue-listings
+ * Domain Path:       /languages
+ */
+defined('ABSPATH') || exit;
