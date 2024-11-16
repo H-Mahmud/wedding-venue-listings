e37889edae46040c22ae445e45ecbcacedb1d18c
Mahmud <me@imahmud.com>
2024-11-16 14:36:58 +0600

    Improve venue list page design

--- templates/listing.php
+++ templates/listing.php
@@ -1,18 +1,20 @@
 <?php
 get_header(); ?>
 
-<div id="primary" class="listings">
+<div id="primary" class="venue-listing">
     <main id="main" class="site-main site-container" role="main">
         <div class="wvl-sidebar">
+
             <form action="<?php echo site_url('listing'); ?>">
                 <div class="wvl-search-form">
-                    <input type="text" name="listing" placeholder="Search for a venue" value="<?php echo get_search_query(); ?>">
+                    <input type="text" name="s" placeholder="Search for a venue" value="<?php echo get_search_query(); ?>">
                     <input type="submit" value="Search">
                 </div>
-
             </form>
+
+
             <div class="wlv-filter">
-                <div class="venue-types">
+                <div class="filter venue-types">
                     <h3>Venue Types</h3>
                     <?php
                     $venue_types = get_terms(array(
@@ -31,7 +33,7 @@ get_header(); ?>
                     }; ?>
                 </div>
 
-                <div class="venue-services">
+                <div class="filter venue-services">
                     <h3>Venue Services</h3>
                     <?php
                     $venue_services = get_terms(array(
@@ -50,7 +52,7 @@ get_header(); ?>
                     }; ?>
                 </div>
 
-                <div class="venue-setting">
+                <div class="filter venue-setting">
                     <h3>Venue Settings</h3>
                     <?php
                     $venue_settings = get_terms(array(
@@ -69,7 +71,7 @@ get_header(); ?>
                     }; ?>
                 </div>
 
-                <div class="availability">
+                <div class="filter availability">
                     <h3>Availability</h3>
                     <input type="date">
                 </div>
@@ -84,37 +86,51 @@ get_header(); ?>
                 'posts_per_page' => -1,
             );
             $query = new WP_Query($args);
+
+            if ($query->have_posts()):
             ?>
+                <p class="total-result"><?php echo $query->found_posts; ?> Venues Found</p>
+                <div class="wvl-list">
+                    <?php
+                    while ($query->have_posts()) :
+                        $query->the_post(); ?>
 
-            <p class="total-result"><?php echo $query->found_posts; ?> Results</p>
-            <div class="wvl-list">
-                <?php
-                while ($query->have_posts()) {
-                    $query->the_post(); ?>
-
-                    <div class="wvl-list-item">
-                        <div class="thumbnail">
-                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Venue Image">
-                        </div>
-                        <div class="wvl-list-item-content">
-                            <h3 class="wvl-list-item-title">
-                                <a href="<?php the_permalink(); ?>">Wedding place name goes here</a>
-                            </h3>
-                            <p>
-                                <span>Location Icon</span>
-                                1901 Thornridge Cir. Shiloh, Hawaii 81063
-                            </p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a </p>
-                            <div class="meta">
-                                <p>Price Range: $1000 - $5000</p>
-                                <p>Rating: 4.7</p>
+                        <div class="wvl-list-item">
+                            <div class="thumbnail">
+                                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Venue Image">
                             </div>
-                            <button>Request a Pricing</button>
+                            <div class="item-content">
+                                <h2 class="item-title">
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h2>
+                                <div>
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    1901 Thornridge Cir. Shiloh, Hawaii 81063
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a </p>
+                                <div class="meta">
+                                    <span>Price Range: <b>$1000 - $5000</b></span>
+                                    <span>
+                                        <span class="stars">
+                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                        </span>
+                                        <span class="amount">(1.8 k+ Reviews)</span>
+                                    </span>
+                                </div>
+                                <a href="<?php the_permalink(); ?>"><button>Request a Pricing</button></a>
                         <?php
-                    }
+                    endwhile;
+                else:
+                    echo '<p>No Venues Found</p>';
+                endif;
+                wp_reset_postdata();
                         ?>
+                            </div>
                         </div>
-                    </div>
     </main>
 </div>
 
