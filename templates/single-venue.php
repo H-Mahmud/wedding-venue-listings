34c5029261bc9f78711ffad85e877303d60f8ebc
Mahmud <me@imahmud.com>
2024-11-16 15:09:46 +0600

    Single venue design

--- templates/single-venue.php
+++ templates/single-venue.php
@@ -1,23 +1,37 @@
 <?php get_header(); ?>
 <div id="primary" class="single-venue">
-    <div class="cover-photo">
-        <!-- <img src="http://koumparos.local/wp-content/uploads/2024/11/মির্জা-গালিব.png" alt=""> -->
-    </div>
-    <main id="main" class="site-main site-container" role="main">
-        <?php
-        while (have_posts()) {
-            the_post(); ?>
+    <?php
+    while (have_posts()) {
+        the_post(); ?>
+        <div class="cover-photo">
+            <?php the_post_thumbnail('full'); ?>
+        </div>
+        <main id="main" class="site-main site-container" role="main">
+
             <div class="header">
                 <div class="entry">
                     <?php the_title('<h1 class="title">', '</h1>'); ?>
                     <div class="meta">
-                        <div class="location"></div>
-                        <div class="reviews"></div>
+                        <span>Price Range: <b>$1000 - $5000</b></span>
+                        <span>
+                            <span class="stars">
+                                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                            </span>
+                            <span class="amount">(1.8 k+ Reviews)</span>
+                        </span>
                     </div>
                 </div>
                 <div class="links">
                     <ul class="social">
-                        <li><a href="">Facebook</a></li>
+                        <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-x-twitter"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-square-youtube"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-linkedin-in"></i></a></li>
                     </ul>
                     <button>Contact Now</button>
                 </div>
@@ -25,10 +39,12 @@
 
             <div class="wvl-content">
                 <ul class="tabs">
-                    <li><a href="">About Place</a></li>
+                    <li class="tab-link active"><a href="">About The Place</a></li>
                 </ul>
 
-                <div class="description"></div>
+                <p class="description">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
 
                 <div class="wvl-gallery">
                     <div class="entry">
@@ -53,8 +69,9 @@
                 </div>
 
             </div>
-        <?php
-        }; ?>
-    </main>
+
+        </main>
+    <?php
+    }; ?>
 </div>
 <?php get_footer(); ?>
