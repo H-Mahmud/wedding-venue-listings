3611cdffd2a342eb91a41f48ea8cb3e5d7cf1234
Mahmud <me@imahmud.com>
2024-11-16 12:26:26 +0600

    Add social media account form

--- template-parts/dashboard/social-accounts.php
+++ template-parts/dashboard/social-accounts.php
@@ -1,1 +1,51 @@
-<h1>Social Accounts</h1>
+<form class="social-accounts-form">
+    <fieldset>
+        <legend>Social Media Accounts</legend>
+        <label for="facebook">Facebook:</label><br>
+        <input type="url" id="facebook" name="facebook" placeholder="https://www.facebook.com/yourprofile">
+        <br><br>
+
+        <label for="twitter">Twitter:</label><br>
+        <input type="url" id="twitter" name="twitter" placeholder="https://www.twitter.com/yourprofile">
+        <br><br>
+
+        <label for="instagram">Instagram:</label><br>
+        <input type="url" id="instagram" name="instagram" placeholder="https://www.instagram.com/yourprofile">
+        <br><br>
+
+        <label for="linkedin">LinkedIn:</label><br>
+        <input type="url" id="linkedin" name="linkedin" placeholder="https://www.linkedin.com/in/yourprofile">
+        <br><br>
+
+        <label for="tiktok">TikTok:</label><br>
+        <input type="url" id="tiktok" name="tiktok" placeholder="https://www.tiktok.com/@yourprofile">
+        <br><br>
+
+        <label for="youtube">YouTube:</label><br>
+        <input type="url" id="youtube" name="youtube" placeholder="https://www.youtube.com/channel/yourchannel">
+        <br><br>
+
+        <label for="pinterest">Pinterest:</label><br>
+        <input type="url" id="pinterest" name="pinterest" placeholder="https://www.pinterest.com/yourprofile">
+        <br><br>
+
+        <label for="snapchat">Snapchat:</label><br>
+        <input type="url" id="snapchat" name="snapchat" placeholder="https://www.snapchat.com/add/yourprofile">
+        <br><br>
+
+        <label for="threads">Threads:</label><br>
+        <input type="url" id="threads" name="threads" placeholder="https://www.threads.net/@yourprofile">
+        <br><br>
+
+        <label for="reddit">Reddit:</label><br>
+        <input type="url" id="reddit" name="reddit" placeholder="https://www.reddit.com/user/yourprofile">
+    </fieldset>
+
+    <!-- Submit Button -->
+    <button type="submit">Submit</button>
+</form>
+<style>
+    .social-accounts-form input {
+        width: 100%;
+    }
+</style>
