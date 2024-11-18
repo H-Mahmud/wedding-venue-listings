a462f01e68439768c62613a193d5037336cd6d87
Mahmud <me@imahmud.com>
2024-11-18 15:44:44 +0600

    Make pretty the application form

--- template-parts/venue-application.php
+++ template-parts/venue-application.php
@@ -1,42 +1,50 @@
 <h1 class="center">Application</h1>
 <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
     <label for="venue_name">
-        Venue Name
+        Venue Name <br>
         <input type="text" name="venue_name" value="<?php echo get_the_title($post_id); ?>" id="venue_name">
     </label>
-
+    <br><br>
     <label for="location">
-        Location
+        Location <br>
         <input type="text" name="location" id="location">
     </label>
+    <br><br>
 
     <label for="social">
-        Social
+        Social <br>
         <input type="text" name="social">
     </label>
+    <br><br>
+
     <label for="phone">
-        Phone
+        Phone <br>
         <input type="phone" name="phone" id="phone">
     </label>
+    <br><br>
 
     <label for="email">
-        Email
+        Email <br>
         <input type="text" name="email" id="email">
     </label>
-
+    <br> <br>
     * Type, Services, Settings
 
     <label for="description">
-        Description
+        Description <br>
         <textarea name="description" id=""></textarea>
     </label>
+    <br> <br>
+
     <label for="cover_photo">
-        Cover Photo
+        Cover Photo <br>
         <input type="file" name="cover_photo" id="cover_photo">
     </label>
+
+    <br> <br>
     <label for="gallery">
-        Gallery
+        Gallery <br>
         <input type="file" name="gallery" multiple id="gallery">
-    </label>
+    </label> <br><br>
     <button type="submit">Submit</button>
 </form>
