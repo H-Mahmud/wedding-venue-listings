63c1a3d4b615609e3f23bc5e2097133eaba8aaec
Mahmud <me@imahmud.com>
2024-11-18 16:03:19 +0600

    remove MUI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import DashboardLayoutBasic from "./pages/dashboard";
 
-// eslint-disable-next-line no-undef
 jQuery(document).ready(function () {
-    // Render your React component instead
     const root = createRoot(document.querySelector("body"));
     root.render(
         <React.StrictMode>
-            <BrowserRouter>
-                <DashboardLayoutBasic />
-            </BrowserRouter>
+            <App />
         </React.StrictMode>
     );
 });
@@ -19,17 +13,6 @@ jQuery(document).ready(function () {
 
 function App() {
     return (
-        <h1>Hello World! <ButtonUsage /></h1>
-    )
-}
-
-
-function ButtonUsage() {
-    return (
-        <ul>
-            {navMenus.map(item => {
-                return (<li><a href={item.slug}>{item.title}</a></li>)
-            })}
-        </ul>
+        <h1>Hello World!</h1>
     )
 }
