f0eed025de939f32d225327622a3c4b8706f4ab5
Mahmud <me@imahmud.com>
2024-11-20 05:07:07 +0600

    Improve contact info page layout

--- template-parts/dashboard/contact-info.php
+++ template-parts/dashboard/contact-info.php
@@ -1,51 +1,90 @@
 <form class="social-accounts-form">
-    <fieldset>
-        <legend>Social Media Accounts</legend>
-        <label for="facebook">Facebook:</label><br>
-        <input type="url" id="facebook" name="facebook" placeholder="https://www.facebook.com/yourprofile">
-        <br><br>
+    <fieldset class="p-5 mb-6 rounded-xl border-gray-200">
+        <legend class="mb-2">Contact Information</legend>
 
-        <label for="twitter">Twitter:</label><br>
-        <input type="url" id="twitter" name="twitter" placeholder="https://www.twitter.com/yourprofile">
-        <br><br>
+        <div class="wvl-field-row">
+            <div class="wvl-field">
+                <label for="phone">Phone</label>
+                <input type="tel" id="phone" name="phone" placeholder="123-456-7890">
+            </div>
 
-        <label for="instagram">Instagram:</label><br>
-        <input type="url" id="instagram" name="instagram" placeholder="https://www.instagram.com/yourprofile">
-        <br><br>
+            <div class="wvl-field">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="BxGQs@example.com">
+            </div>
+        </div>
 
-        <label for="linkedin">LinkedIn:</label><br>
-        <input type="url" id="linkedin" name="linkedin" placeholder="https://www.linkedin.com/in/yourprofile">
-        <br><br>
+        <div class="wvl-field-row">
+            <div class="wvl-field">
+                <label for="website">Website</label>
+                <input type="url" id="website" name="website" placeholder="https://www.example.com">
+            </div>
 
-        <label for="tiktok">TikTok:</label><br>
-        <input type="url" id="tiktok" name="tiktok" placeholder="https://www.tiktok.com/@yourprofile">
-        <br><br>
-
-        <label for="youtube">YouTube:</label><br>
-        <input type="url" id="youtube" name="youtube" placeholder="https://www.youtube.com/channel/yourchannel">
-        <br><br>
-
-        <label for="pinterest">Pinterest:</label><br>
-        <input type="url" id="pinterest" name="pinterest" placeholder="https://www.pinterest.com/yourprofile">
-        <br><br>
-
-        <label for="snapchat">Snapchat:</label><br>
-        <input type="url" id="snapchat" name="snapchat" placeholder="https://www.snapchat.com/add/yourprofile">
-        <br><br>
-
-        <label for="threads">Threads:</label><br>
-        <input type="url" id="threads" name="threads" placeholder="https://www.threads.net/@yourprofile">
-        <br><br>
-
-        <label for="reddit">Reddit:</label><br>
-        <input type="url" id="reddit" name="reddit" placeholder="https://www.reddit.com/user/yourprofile">
+            <div class="wvl-field">
+                <label for="address">Address</label>
+                <input type="text" id="address" name="address" placeholder="123 Main St, Anytown, USA">
+            </div>
+        </div>
     </fieldset>
 
-    <!-- Submit Button -->
-    <button type="submit">Submit</button>
+
+
+    <fieldset class="p-5 rounded-xl border-gray-200">
+        <legend class="mb-2"><?php _e('Social Media Accounts', 'wedding-venue-listings'); ?></legend>
+
+        <div class="wvl-field">
+            <label for="facebook">Facebook</label>
+            <input type="url" id="facebook" name="facebook" placeholder="https://www.facebook.com/yourprofile">
+        </div>
+
+        <div class="wvl-field">
+            <label for="twitter">Twitter</label>
+            <input type="url" id="twitter" name="twitter" placeholder="https://www.twitter.com/yourprofile">
+        </div>
+
+        <div class="wvl-field">
+            <label for="instagram">Instagram</label>
+            <input type="url" id="instagram" name="instagram" placeholder="https://www.instagram.com/yourprofile">
+        </div>
+
+
+
+        <div class="wvl-field">
+            <label for="linkedin">LinkedIn</label>
+            <input type="url" id="linkedin" name="linkedin" placeholder="https://www.linkedin.com/in/yourprofile">
+        </div>
+
+
+        <div class="wvl-field">
+            <label for="tiktok">TikTok</label>
+            <input type="url" id="tiktok" name="tiktok" placeholder="https://www.tiktok.com/@yourprofile">
+        </div>
+
+        <div class="wvl-field">
+            <label for="youtube">YouTube</label>
+            <input type="url" id="youtube" name="youtube" placeholder="https://www.youtube.com/channel/yourchannel">
+        </div>
+
+        <div class="wvl-field">
+            <label for="pinterest">Pinterest</label>
+            <input type="url" id="pinterest" name="pinterest" placeholder="https://www.pinterest.com/yourprofile">
+        </div>
+
+        <div class="wvl-field">
+            <label for="snapchat">Snapchat</label>
+            <input type="url" id="snapchat" name="snapchat" placeholder="https://www.snapchat.com/add/yourprofile">
+        </div>
+
+        <div class="wvl-field">
+            <label for="threads">Threads</label>
+            <input type="url" id="threads" name="threads" placeholder="https://www.threads.net/@yourprofile">
+        </div>
+
+        <div class="wvl-field">
+            <label for="reddit">Reddit</label><br>
+            <input type="url" id="reddit" name="reddit" placeholder="https://www.reddit.com/user/yourprofile">
+        </div>
+    </fieldset>
+
+    <button type="submit" class="wvl-btn-primary">Save</button>
 </form>
-<style>
-    .social-accounts-form input {
-        width: 100%;
-    }
-</style>
