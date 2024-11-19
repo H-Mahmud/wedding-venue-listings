ac04217cf79cfabe026ad62bee0a121fccb6d22e
Mahmud <me@imahmud.com>
2024-11-20 04:26:46 +0600

    Improve password reset page

--- template-parts/dashboard/account.php
+++ template-parts/dashboard/account.php
@@ -1,21 +1,36 @@
-<form>
-    <fieldset>
-        <legend>General Information</legend>
-        <label for="fistName">First Name:</label><br>
-        <input type="text" id="fistName" name="first_name" placeholder="John" required>
-        <br><br>
-        <label for="lastName">Last Name:</label><br>
-        <input type="text" id="lastName" name="last_name" placeholder="Doe" required>
-    </fieldset>
+<form class=" mb-14">
+    <fieldset class="p-5 rounded-xl border-gray-200">
+        <legend class="mb-2">General Information</legend>
+        <div class="wvl-field-row">
+            <div class="wvl-field">
+                <label for="fistName">First Name:</label>
+                <input type="text" id="fistName" name="first_name" placeholder="John" required>
+            </div>
 
-    <fieldset>
-        <legend>Password Reset</legend>
-        <label for="newPassword">New Password:</label><br>
-        <input type="password" id="newPassword" name="new_password" required>
-        <br><br>
-
-        <label for="confirmPassword">Email Address:</label><br>
-        <input type="password" id="confirmPassword" name="confirm_password" required>
+            <div class="wvl-field">
+                <label for="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="last_name" placeholder="Doe" required>
+            </div>
+        </div>
     </fieldset>
-    <button type="submit">Submit</button>
+    <button type="submit" class="wvl-btn-primary">Save</button>
 </form>
+
+<form>
+    <fieldset class="p-5 rounded-xl border-gray-200">
+        <legend>Password Reset</legend>
+
+        <div class="wvl-field-row">
+            <div class="wvl-field">
+                <label for="newPassword">New Password</label>
+                <input type="password" id="newPassword" name="new_password" required>
+            </div>
+
+            <div class="wvl-field">
+                <label for="confirmPassword">Confirm Password</label>
+                <input type="password" id="confirmPassword" name="confirm_password" required>
+            </div>
+        </div>
+    </fieldset>
+    <button type="submit" class="wvl-btn-primary">Submit</button>
+</form
