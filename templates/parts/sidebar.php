cda13d70afb427bc8708fa7d71cbfd459985ee48
Mahmud <me@imahmud.com>
2024-12-16 01:03:28 +0600

    Add filter other toggler

--- templates/parts/sidebar.php
+++ templates/parts/sidebar.php
@@ -18,24 +18,16 @@
                 if ($category->parent == 0) {
                     $checked = isset($_GET['category']) && $_GET['category'] == $category->term_id ? 'checked="checked"' : '';
                     echo <<<HTML
-                                <label class="name">
-                                    <input class="radio" type="radio" name="category" value="$category->term_id" $checked />
-                                    $category->name
-                                </label>
-                                <br>
-                            HTML;
+                        <label class="name">
+                            <input class="radio" type="radio" name="category" value="$category->term_id" $checked />
+                            $category->name
+                        </label>
+                        <br>
+                    HTML;
                 }
-            }; ?>
+            };
+            ?>
         </div>
-        <script>
-            jQuery(document).ready(function($) {
-                $('.listing-archive .filter .header .toggle-icon').on('click', function() {
-                    $(this).toggleClass('fa-angle-up');
-                    $(this).toggleClass('fa-angle-down');
-                    $('.filter .content').slideToggle();
-                })
-            })
-        </script>
     </form>
     <?php
     if (isset($_GET['category']) && !empty($_GET['category']) && is_numeric($_GET['category'])):
@@ -46,31 +38,49 @@
             'taxonomy'   => 'category',
         ];
     ?>
-        <form class="filter subcategory mb-5" id="subcategoryFilter" method="get">
-            <h3 class="text-lg font-bold"><?php _e('Sub Category', 'wedding-venue-listings'); ?></h3>
-            <?php
-            $subcategories = get_terms($args);
-            $selected_subcategories = [];
-            if (isset($_GET['subcategory']) && !empty($_GET['subcategory']) && is_array($_GET['subcategory'])) {
-                $selected_subcategories = $_GET['subcategory'];
-            }
-            foreach ($subcategories as $subcategory) {
-                $checked = in_array($subcategory->term_id, $selected_subcategories) ? 'checked="checked"' : '';
-                echo <<<HTML
-                        <label class="my-2 inline-block cursor-pointer">
-                            <input class="mr-2 w-4 h-4" type="checkbox" name="subcategory[]" value="$subcategory->term_id" $checked />
+        <form class="filter subcategory" id="subcategoryFilter" method="get">
+            <div class="header">
+                <h3 class="title"><?php _e('Subcategory', 'wedding-venue-listings'); ?></h3>
+                <i class="fa-solid fa-angle-up toggle-icon"></i>
+            </div>
+            <div class="content">
+                <?php
+                $subcategories = get_terms($args);
+                $selected_subcategories = [];
+                if (isset($_GET['subcategory']) && !empty($_GET['subcategory']) && is_array($_GET['subcategory'])) {
+                    $selected_subcategories = $_GET['subcategory'];
+                }
+                foreach ($subcategories as $subcategory) {
+                    $checked = in_array($subcategory->term_id, $selected_subcategories) ? 'checked="checked"' : '';
+                    echo <<<HTML
+                        <label class="name">
+                            <input class="checkbox" type="checkbox" name="subcategory[]" value="$subcategory->term_id" $checked />
                             $subcategory->name
                         </label>
                         <br>
                     HTML;
-            }; ?>
+                }; ?>
+            </div>
         </form>
+
+        <script>
+            jQuery(document).ready(function($) {
+                $('.listing-archive .filter .header .toggle-icon').on('click', function() {
+                    $(this).toggleClass('fa-angle-up');
+                    $(this).toggleClass('fa-angle-down');
+                    $(this).closest('.filter').find('.content').slideToggle();
+                })
+            })
+        </script>
     <?php endif; ?>
 
     <div class="filter wvl-field availability mb-4">
-        <h3 class="text-lg font-bold">Availability</h3>
+        <div class="header">
+            <h3 class="title"><?php _e('Availability', 'wedding-venue-listings'); ?></h3>
+            <i class="fa-solid fa-angle-up toggle-icon"></i>
+        </div>
 
-        <div class="wvl-calendar">
+        <div class="content wvl-calendar">
             <div class="calendar-header">
                 <button id="prev-month">&lt;</button>
                 <span id="month-year"></span>
