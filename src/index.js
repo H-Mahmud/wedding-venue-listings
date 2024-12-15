20d39ea385ee38c4240866a3789e318361f16aa8
Mahmud <me@imahmud.com>
2024-12-16 01:03:59 +0600

    Enqueue other scripts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-// import './dev-env'; // Development only
-
 // Components
-// import './components/calendar';
-// import './components/star-rating';
-// import './components/modal';
+import './components/calendar';
+import './components/star-rating';
+import './components/modal';
 
 // Styles
 import "./style.scss";
-// import "./styles/forms.css";
+import "./styles/forms.css";
 import './styles/listing-page.scss';
 
 
-// import './styles/listing-archive.scss';
+import './styles/listing-archive.scss';
