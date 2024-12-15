b16d4e78f6989071947398fb35f5138d5d49e4a4
Mahmud <me@imahmud.com>
2024-12-16 00:54:01 +0600

    Refactor listing page style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // import './dev-env'; // Development only
 
 // Components
-import './components/calendar';
-import './components/star-rating';
-import './components/modal';
+// import './components/calendar';
+// import './components/star-rating';
+// import './components/modal';
 
 // Styles
 import "./style.scss";
-import "./styles/forms.css";
-import './styles/listing-page.css';
+// import "./styles/forms.css";
+import './styles/listing-page.scss';
 
 
-import './styles/listing-archive.scss';
+// import './styles/listing-archive.scss';
