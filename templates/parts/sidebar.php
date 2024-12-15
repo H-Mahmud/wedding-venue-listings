abff887ec9c284e6d1bef70f2a45e449aa544019
Mahmud <me@imahmud.com>
2024-12-15 23:37:06 +0600

    Refactor: Remove js input checked

--- templates/parts/sidebar.php
+++ templates/parts/sidebar.php
@@ -36,10 +36,15 @@
             <h3 class="text-lg font-bold"><?php _e('Sub Category', 'wedding-venue-listings'); ?></h3>
             <?php
             $subcategories = get_terms($args);
+            $selected_subcategories = [];
+            if (isset($_GET['subcategory']) && !empty($_GET['subcategory']) && is_array($_GET['subcategory'])) {
+                $selected_subcategories = $_GET['subcategory'];
+            }
             foreach ($subcategories as $subcategory) {
+                $checked = in_array($subcategory->term_id, $selected_subcategories) ? 'checked="checked"' : '';
                 echo <<<HTML
                         <label class="my-2 inline-block cursor-pointer">
-                            <input class="mr-2 w-4 h-4" type="checkbox" name="subcategory[]" value="$subcategory->term_id" />
+                            <input class="mr-2 w-4 h-4" type="checkbox" name="subcategory[]" value="$subcategory->term_id" $checked />
                             $subcategory->name
                         </label>
                         <br>
@@ -72,85 +77,4 @@
             </div>
         </div>
     </div>
-
-    <script>
-        jQuery(document).ready(function($) {
-
-            $('#categoryFilter input[name="category"]').on('change', function() {
-                $('#categoryFilter').submit();
-            });
-
-            $('#subcategoryFilter input[name="subcategory[]"]').on('change', function() {
-                const params = new URLSearchParams(window.location.search);
-                params.delete('subcategory[]');
-
-                $('input[name="subcategory[]"]:checked').each(function() {
-                    params.append('subcategory[]', $(this).val());
-                });
-                const newUrl = `${window.location.pathname}?${params.toString()}`;
-                window.history.replaceState({}, '', newUrl);
-                window.location.href = newUrl;
-            });
-
-            const params = new URLSearchParams(window.location.search);
-            $('#subcategoryFilter input[name="subcategory[]"]').each(function() {
-                if (params.getAll('subcategory[]').includes($(this).val())) {
-                    $(this).prop('checked', true);
-                }
-            });
-
-            // const params = new URLSearchParams(window.location.search);
-            // $('#categoryFilter input[name="category"]').each(function() {
-            //     if (params.getAll('category').includes($(this).val())) {
-            //         $(this).prop('checked', true);
-            //     }
-            // });
-
-            <?php
-            /*
-                        // vendor types
-                        $('#vendorTypesFilter input[name="vendor-types[]"]').on('change', function() {
-                            const params = new URLSearchParams(window.location.search);
-                            params.delete('vendor-types[]');
-
-                            $('input[name="vendor-types[]"]:checked').each(function() {
-                                params.append('vendor-types[]', $(this).val());
-                            });
-                            const newUrl = `${window.location.pathname}?${params.toString()}`;
-                            window.history.replaceState({}, '', newUrl);
-                            window.location.href = newUrl;
-                        });
-
-                        const params = new URLSearchParams(window.location.search);
-                        $('#vendorTypesFilter input[name="vendor-types[]"]').each(function() {
-                            if (params.getAll('vendor-types[]').includes($(this).val())) {
-                                $(this).prop('checked', true);
-                            }
-                        });
-
-
-                        // event types
-                        $('#eventTypesFilter input[name="event-types[]"]').on('change', function() {
-                            const params = new URLSearchParams(window.location.search);
-                            params.delete('event-types[]');
-
-                            $('input[name="event-types[]"]:checked').each(function() {
-                                params.append('event-types[]', $(this).val());
-                            });
-                            const newUrl = `${window.location.pathname}?${params.toString()}`;
-                            window.history.replaceState({}, '', newUrl);
-                            window.location.href = newUrl;
-                        });
-
-                        $('#eventTypesFilter input[name="event-types[]"]').each(function() {
-                            if (params.getAll('event-types[]').includes($(this).val())) {
-                                $(this).prop('checked', true);
-                            }
-                        });
-
-                        */
-            ?>
-        });
-    </script>
-
 </div>
