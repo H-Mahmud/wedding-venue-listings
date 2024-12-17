818f653a925028d3d2294041e037295334e49419
Mahmud <me@imahmud.com>
2024-12-17 22:55:38 +0600

    Refactor data storage method

--- includes/analytics/class-analytic-data-storage.php
+++ includes/analytics/class-analytic-data-storage.php
@@ -45,12 +45,32 @@ class WVL_Analytic_Data_Storage
         ));
     }
 
+    /**
+     * Retrieves the count of daily analytics entries for a given venue ID and event type
+     *
+     * @param int $venue_id The post ID of the venue
+     * @param string $event_type The type of event to log, one of impression, view, unique_view, contact_click, lead
+     *
+     * @return int The count of daily analytics entries
+     */
     public static function get_daily_data_count($venue_id, $event_type)
     {
         global $wpdb;
         $table_name = $wpdb->prefix . 'venue_daily_analytics';
         return $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE venue_id = %d AND event_type = %s", $venue_id, $event_type));
     }
+
+    /**
+     * Retrieves the count of unique daily analytics entries for a given venue ID and event type.
+     *
+     * This function counts the distinct IP addresses associated with the specified
+     * venue ID and event type, providing a measure of unique interactions.
+     *
+     * @param int $venue_id The post ID of the venue.
+     * @param string $event_type The type of event to log, one of impression, view, unique_view, contact_click, lead.
+     *
+     * @return int The count of unique daily analytics entries.
+     */
 
     public static function get_daily_data_unique_count($venue_id, $event_type)
     {
@@ -59,6 +79,14 @@ class WVL_Analytic_Data_Storage
         return $wpdb->get_var($wpdb->prepare("SELECT COUNT(DISTINCT ip_address) FROM $table_name WHERE venue_id = %d AND event_type = %s", $venue_id, $event_type));
     }
 
+    /**
+     * Deletes daily analytics entries for a given venue ID and event type.
+     *
+     * @param int $venue_id The post ID of the venue.
+     * @param string $event_type The type of event to log, one of impression, view, unique_view, contact_click, lead.
+     *
+     * @return void
+     */
     public static function delete_daily_data($venue_id, $event_type)
     {
         global $wpdb;
