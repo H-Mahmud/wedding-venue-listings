3d743eec05a36fe9c200c1b65286ebf69964ee48
Mahmud <me@imahmud.com>
2024-12-17 15:39:38 +0600

    Get user current subscription plan

--- classes/class-subscription-manage.php
+++ classes/class-subscription-manage.php
@@ -42,7 +42,38 @@ class WVL_Subscription_Manage
 }
 WVL_Subscription_Manage::get_instance();
 
-function wvl_current_plan()
+
+/**
+ * Determines the current subscription plan for a user.
+ *
+ * This function retrieves all active subscriptions for a user and checks their status.
+ * It returns the slug of the highest-level active subscription plan ('ultimate' or 'pro').
+ * If no active subscriptions are found, it defaults to returning 'free'.
+ *
+ * @return string The slug of the current subscription plan ('ultimate', 'pro', or 'free').
+ */
+
+function wvl_current_plan($user_id = 0)
 {
-    return 'ultimate';
-};
+    if (!method_exists('\Indeed\Ihc\UserSubscriptions', 'getAllForUser'))  return 'free';
+
+    if (!$user_id) $user_id = get_current_user_id();
+
+    $active_subscriptions = \Indeed\Ihc\UserSubscriptions::getAllForUser($user_id);
+    if (!is_array($active_subscriptions) || count($active_subscriptions) == 0)  return 'free';
+
+    $slug = 'free';
+
+    foreach ($active_subscriptions as $subscription) {
+
+        if ($subscription['status'] != 1)  continue;
+
+        if ($subscription['level_slug'] == 'ultimate') {
+            $slug = $subscription['level_slug'];
+        } elseif ($subscription['level_slug'] == 'pro' && $slug !== 'ultimate') {
+            $slug = $subscription['level_slug'];
+        }
+    }
+
+    return $slug;
+}
