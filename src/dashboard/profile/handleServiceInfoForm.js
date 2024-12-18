44cb7231781eb854d8cc805d40f8d54c5e4a9d0d
Mahmud <me@imahmud.com>
2024-12-18 17:41:36 +0600

    Refactor step indicator

--- src/dashboard/profile/handleServiceInfoForm.js
+++ src/dashboard/profile/handleServiceInfoForm.js
@@ -1,64 +1,63 @@
 import { hideLoading, showLoading } from "./utils";
 
 export default function handleServiceInfoForm($) {
-    const $form = $('form#profileServiceForm');
-    const $profileFormError = $('.profile-form-error')
+  const $form = $("form#profileServiceForm");
+  const $profileFormError = $(".profile-form-error");
 
-    if (!$form.get(0).checkValidity()) {
-        $form.get(0).reportValidity();
-        return false;
-    }
+  if (!$form.get(0).checkValidity()) {
+    $form.get(0).reportValidity();
+    return false;
+  }
 
-    const $venueName = $form.find('#venue_name').val();
-    // const $vendorType = $form.find('#vendor_type').val();
-    // const $eventType = $form.find('#event_type').val();
-    const $category = $form.find('#category').val();
-    const $subCategory = $form.find('#subcategory').val();
-    const $supportLocation = $form.find('#support_location').val();
+  const $venueName = $form.find("#venue_name").val();
+  // const $vendorType = $form.find('#vendor_type').val();
+  // const $eventType = $form.find('#event_type').val();
+  const $category = $form.find("#category").val();
+  const $subCategory = $form.find("#subcategory").val();
+  const $supportLocation = $form.find("#support_location").val();
 
+  const formData = {
+    action: "submit_profile_service_info",
+    nonce: WVL_DATA.ajax_nonce,
+    venue_name: $venueName,
+    // vendor_type: $vendorType,
+    // event_type: $eventType,
+    category: $category,
+    sub_category: $subCategory,
+    support_location: $supportLocation,
+  };
 
-    const formData = {
-        action: 'submit_profile_service_info',
-        nonce: WVL_DATA.ajax_nonce,
-        venue_name: $venueName,
-        // vendor_type: $vendorType,
-        // event_type: $eventType,
-        category: $category,
-        sub_category: $subCategory,
-        support_location: $supportLocation
-    }
+  console.log(formData);
 
-    // console.log(formData);
+  if (!$venueName || !$category || !$subCategory || !$supportLocation) {
+    $profileFormError.html("All fields are required.");
+    return false;
+  }
 
-    if (!$venueName || !$category || !$subCategory ||  !$supportLocation) {
-        $profileFormError.html("All fields are required.");
-        return false;
-    }
-
-    showLoading();
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: WVL_DATA.ajax_url,
-            type: 'POST',
-            dataType: 'json',
-            data: formData,
-            success: function (response) {
-                if (response.success) {
-                    $profileFormError.html("");
-                    resolve(true);
-                } else {
-                    $profileFormError.html(response.data.message || "An error occurred.");
-                    resolve(false);
-                }
-            },
-            error: function (xhr, status, error) {
-                console.error("AJAX Error: ", status, error);
-                $profileFormError.html("Something went wrong. Please try again later.");
-                resolve(false);
-            },
-            complete: function (data) {
-                hideLoading();
-            }
-        });
+  showLoading();
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: WVL_DATA.ajax_url,
+      type: "POST",
+      dataType: "json",
+      data: formData,
+      success: function (response) {
+        if (response.success) {
+          $profileFormError.html("");
+          resolve(true);
+        } else {
+          $profileFormError.html(response.data.message || "An error occurred.");
+          resolve(false);
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error("AJAX Error: ", status, error);
+        $profileFormError.html("Something went wrong. Please try again later.");
+        resolve(false);
+      },
+      complete: function (data) {
+        hideLoading();
+      },
     });
+  });
 }
---
+++ src/dashboard/profile/stepIndicator.js
@@ -0,0 +1,83 @@
+export default class StepIndicator {
+  /**
+   * @param el CSS selector of the step indicator element
+   */
+  constructor(el, handleNext = async () => {}, currentStep = 0) {
+    this.steps = 6;
+    this._step = currentStep;
+    this.$el = jQuery(el);
+    this.handleNext = handleNext;
+
+    jQuery(document).on("click", '[data-action="prev"]', this.prev.bind(this));
+    jQuery(document).on("click", '[data-action="next"]', this.next.bind(this));
+
+    this.displayStep(this.step);
+    this.checkExtremes();
+  }
+
+  get step() {
+    return this._step;
+  }
+
+  set step(value) {
+    this._step = value;
+    this.displayStep(value);
+    this.checkExtremes();
+  }
+
+  /** Go to the previous step. */
+  prev() {
+    if (this.step > 0) {
+      this.step -= 1;
+    }
+  }
+
+  /** Go to the next step. */
+  async next() {
+    if (!(await this.handleNext(this.step))) return;
+    if (this.step < this.steps - 1) {
+      this.step += 1;
+
+      const url = new URL(window.location.href);
+      url.searchParams.set("step", this.step);
+      window.history.pushState({}, "", url.href);
+    }
+  }
+
+  displayStep(targetStep) {
+    const currentClass = "steps__step--current";
+    const doneClass = "steps__step--done";
+
+    jQuery(".profile-step-forms").removeClass("active");
+    jQuery('.profile-step-forms[data-step="' + targetStep + '"]').addClass(
+      "active",
+    );
+
+    for (let s = 0; s < this.steps; ++s) {
+      const $stepEl = this.$el.find(`[data-step="${s}"]`);
+      $stepEl.removeClass(`${currentClass} ${doneClass}`);
+      jQuery('form[data-step="' + s + '"]').hide();
+      if (s < targetStep) {
+        $stepEl.addClass(doneClass);
+      } else if (s === targetStep) {
+        $stepEl.addClass(currentClass);
+      }
+    }
+  } /** Disable the Previous or Next button if hitting the first or last step. */
+  checkExtremes() {
+    const $prevBtn = jQuery('[data-action="prev"]');
+    const $nextBtn = jQuery('[data-action="next"]');
+    $prevBtn.prop("disabled", this.step <= 0);
+    if (WVL_DATA.venue_status === "publish") {
+      $nextBtn.html(this.step >= this.steps - 1 ? "Save" : "Next");
+      // $nextBtn.prop("disabled", this.step >= this.steps - 1);
+    } else if (WVL_DATA.venue_status === "pending") {
+      $nextBtn.prop("disabled", this.step >= this.steps - 1);
+      $nextBtn.html(this.step >= this.steps - 1 ? "Submitted" : "Next");
+    } else {
+      $nextBtn.html(
+        this.step >= this.steps - 1 ? "Request to Publish" : "Next",
+      );
+    }
+  }
+}
